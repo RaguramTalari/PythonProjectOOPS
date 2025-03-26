3e81039f71af50434dc8242970646d289901fe84
RaguramTalari <raguramtalari150@gmail.com>
2025-03-26 06:50:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-from Class_2 import Student,Teacher,Counsellor,Courses,CareerPath,Session,User,create_user,list_teachers,list_counsellors
+from db_connector import get_db_connection as gt
+from Class_2 import Student,Teacher,Counsellor,Courses,CareerPath,Session,User,create_user,list_teachers,list_counsellors,update_user_details
 if __name__ == "__main__":
     print("_________________WELCOME TO JEET CAREER GUIDANCE PROGRAM__________________")
     while(1):
-        a=int(input("1.Add a User -------- 2.Quit ::"))
-        if(a==1):
-            create_user()
-            conn=gt()
-            cursor = conn.cursor()
-            cursor.close()
-            conn.close()
-        if(a==2):
-            print("THANKYOU")
-            break
-        else:
-            print("invalid number try again")
+       print("1.ADD USER \n ")
+       print("2.UPDATE USER \n")
+       print("3.QUIT\n")
+       a=int(input("Enter the number"))
+       if(a==1):
+           create_user()
+           conn=gt()
+           cursor = conn.cursor()
+           cursor.close()
+           conn.close()
+       elif(a==2):
+           conn= gt()
+           cursor = conn.cursor()
+           update_user_details()
+           cursor.close()
+           conn.close()
+       elif(a==3):
+           conn = gt()
+           cursor = conn.cursor()
+           c=User.fetch_all_users()
+           for i in c:
+               print(i)
+           cursor.close()
+           conn.close()
+           print("THANKYOU")
+           break
+       else:
+           print("invalid number try again")
